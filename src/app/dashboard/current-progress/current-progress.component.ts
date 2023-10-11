80ecc8edbdd4724a438cca868af037b3aacedb11
Magda Paicu <magdalena.paicu@ness.com>
2023-10-11 08:14:23 +0300

    feat(current-progress): create updateChart method

--- src/app/dashboard/current-progress/current-progress.component.ts
+++ src/app/dashboard/current-progress/current-progress.component.ts
@@ -7,13 +7,16 @@ declare var google: any;
   styleUrls: ['./current-progress.component.scss'],
 })
 export class CurrentProgressComponent implements OnInit {
+  selectedLastDays: string = '';
+  currentHoverValue: string = '';
+  chartData: any = [];
+
   lastDays: string[] = [
     'Last 7 Days',
     'Last 14 Days',
     'Last 21 Days',
     'Last 30 Days',
   ];
-  selectedLastDays: string = '';
 
   legendItems = [
     { name: 'Register', color: '#00195f' },
@@ -22,21 +25,61 @@ export class CurrentProgressComponent implements OnInit {
     { name: 'Not Started', color: '#CCD1DF' },
   ];
 
-  currentHoverValue: string = '';
+  dataForLast7Days = [
+    ['Task', 'Hours per Day'],
+    ['Register', 10],
+    ['Finished', 80],
+    ['In Progress', 5],
+    ['Not Started', 5],
+  ];
+
+  dataForLast14Days = [
+    ['Task', 'Hours per Day'],
+    ['Register', 15],
+    ['Finished', 30],
+    ['In Progress', 10],
+    ['Not Started', 45],
+  ];
+
+  dataForLast21Days = [
+    ['Task', 'Hours per Day'],
+    ['Register', 1],
+    ['Finished', 2],
+    ['In Progress', 90],
+    ['Not Started', 7],
+  ];
+
+  dataForLast30Days = [
+    ['Task', 'Hours per Day'],
+    ['Register', 25],
+    ['Finished', 25],
+    ['In Progress', 25],
+    ['Not Started', 25],
+  ];
 
   ngOnInit(): void {
+    this.chartData = this.dataForLast7Days;
+    this.selectedLastDays = this.lastDays[0];
     google.charts.load('current', { packages: ['corechart'] });
     google.charts.setOnLoadCallback(() => this.drawChart());
   }
+  updateChart() {
+    if (this.selectedLastDays === 'Last 7 Days') {
+      this.chartData = this.dataForLast7Days;
+    } else if (this.selectedLastDays === 'Last 14 Days') {
+      this.chartData = this.dataForLast14Days;
+    } else if (this.selectedLastDays === 'Last 21 Days') {
+      this.chartData = this.dataForLast21Days;
+    } else if (this.selectedLastDays === 'Last 30 Days') {
+      this.chartData = this.dataForLast30Days;
+    } else {
+      this.chartData = [];
+    }
+    this.drawChart();
+  }
 
   drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Task', 'Hours per Day'],
-      ['Register', 17],
-      ['Finished', 3],
-      ['In Progress', 10],
-      ['Not Started', 70],
-    ]);
+    var data = google.visualization.arrayToDataTable(this.chartData);
 
     var options = {
       pieHole: 0.78,
@@ -49,18 +92,18 @@ export class CurrentProgressComponent implements OnInit {
     var chart = new google.visualization.PieChart(
       document.getElementById('donutchart')
     );
+
     google.visualization.events.addListener(
       chart,
       'onmouseover',
       (eventData: { row: any }) => {
         var sliceIndex = eventData.row;
         var sliceValue = data.getValue(sliceIndex, 1);
-
         this.currentHoverValue = `${sliceValue}%`;
-
         console.log(this.currentHoverValue);
       }
     );
+
     google.visualization.events.addListener(chart, 'onmouseout', () => {});
     chart.draw(data, options);
   }
