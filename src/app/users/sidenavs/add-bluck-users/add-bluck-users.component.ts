10b3b9c1d77670bf16a58b6b258597efa34ad334
Magda Paicu <magdalena.paicu@ness.com>
2023-10-20 12:33:40 +0300

    feat(add-bulk-users):create sidenav component

--- src/app/users/sidenavs/add-bluck-users/add-bluck-users.component.ts
+++ src/app/users/sidenavs/add-bluck-users/add-bluck-users.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-add-bluck-users',
   templateUrl: './add-bluck-users.component.html',
-  styleUrls: ['./add-bluck-users.component.scss']
+  styleUrls: ['./add-bluck-users.component.scss'],
 })
 export class AddBluckUsersComponent {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
+  openSidenav() {
+    this.sidenav.open();
+  }
+
+  closeSidenav() {
+    this.sidenav.close();
+  }
 }
