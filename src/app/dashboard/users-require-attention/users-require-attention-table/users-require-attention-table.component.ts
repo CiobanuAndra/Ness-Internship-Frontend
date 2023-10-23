d5553c143debbcf2211b8274618a888b9eac74c3
Magda Paicu <magdalena.paicu@ness.com>
2023-10-23 16:49:46 +0300

    feat(users-require-attention-table):added table using Angular Material

--- src/app/dashboard/users-require-attention/users-require-attention-table/users-require-attention-table.component.ts
+++ src/app/dashboard/users-require-attention/users-require-attention-table/users-require-attention-table.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-users-require-attention-table',
   templateUrl: './users-require-attention-table.component.html',
-  styleUrls: ['./users-require-attention-table.component.scss']
+  styleUrls: ['./users-require-attention-table.component.scss'],
 })
-export class UsersRequireAttentionTableComponent {
+export class UsersRequireAttentionTableComponent implements OnInit {
+  constructor(private usersService: UsersService) {}
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+  ngOnInit() {}
 }
