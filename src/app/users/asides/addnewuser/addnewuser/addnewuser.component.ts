39cd47e69f006be124ac2791bf8b294b6a9f878e
FlorinBodogan <florin.bodogan@ness.com>
2023-10-27 12:48:45 +0300

    feat(addnewuser): initial skeleton

--- src/app/users/asides/addnewuser/addnewuser/addnewuser.component.ts
+++ src/app/users/asides/addnewuser/addnewuser/addnewuser.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ResourcesService } from 'src/app/services/resources/resources.service';
 
 @Component({
@@ -10,11 +11,21 @@ export class AddnewuserComponent {
   @Input() showSidenav!: boolean;
   @Output() showSidenavChange = new EventEmitter<boolean>();
 
-  constructor(private resourcesService: ResourcesService) {}
+  addNewUserForm = this.formBuilder.group({
+    name: ['', Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    surname: ['', Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    email: ['', Validators.required, Validators.email],
+  });
+
+  constructor(private resourcesService: ResourcesService, private formBuilder:FormBuilder) {}
 
   closeAside(): void {
     this.showSidenav = false;
     this.showSidenavChange.emit(this.showSidenav);
     this.resourcesService.setSidenavVisibility(false);
+  }
+
+  onSubmit():void {
+    console.log('submitted form', this.addNewUserForm.value);
   }
 }
