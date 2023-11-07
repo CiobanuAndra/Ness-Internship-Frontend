a500345a2003398c49376e751cb2419fd13b61c7
FlorinBodogan <florin.bodogan@ness.com>
2023-11-07 10:28:36 +0200

    refactor(add-new-user-aside): fixed minor space gap

--- src/app/utils/formUtils.ts
+++ src/app/utils/formUtils.ts
@@ -2,11 +2,13 @@ import { AbstractControl, Validators } from '@angular/forms';
 
 function emailDomain(control: AbstractControl): { [key: string]: boolean } | null {
   const domain = control.value ? control.value.substring(control.value.lastIndexOf('@') + 1) : null;
+
   return domain && domain.toLowerCase() === 'ness.com' ? null : { 'emailDomain': true };
 };
 
 function specialChars(control: AbstractControl): { [key: string]: boolean } | null {
   const specialChars = /[!@#$%^&*`()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  
   return (control.value && specialChars.test(control.value)) ? { 'specialChars': true } : null;
 };
 
