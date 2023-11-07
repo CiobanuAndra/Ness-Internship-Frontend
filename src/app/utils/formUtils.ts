0e8ebf1db97ec3716e35d33374d1120c973719a8
FlorinBodogan <florin.bodogan@ness.com>
2023-11-07 10:29:46 +0200

    refactor(add-new-user-aside): fixed minor thing

--- src/app/utils/formUtils.ts
+++ src/app/utils/formUtils.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 function emailDomain(control: AbstractControl): { [key: string]: boolean } | null {
   const domain = control.value ? control.value.substring(control.value.lastIndexOf('@') + 1) : null;
@@ -8,7 +8,7 @@ function emailDomain(control: AbstractControl): { [key: string]: boolean } | nul
 
 function specialChars(control: AbstractControl): { [key: string]: boolean } | null {
   const specialChars = /[!@#$%^&*`()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  
+
   return (control.value && specialChars.test(control.value)) ? { 'specialChars': true } : null;
 };
 
