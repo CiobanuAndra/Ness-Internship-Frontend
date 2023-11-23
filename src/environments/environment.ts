e6a4a87390026899ccc1a156819883f55b55b6ee
Eduard Rosu <eduard.rosu@ness.com>
2023-11-23 09:30:07 +0200

    build: adds pro url for gamification service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUserURL: 'http://ec2-3-231-211-65.compute-1.amazonaws.com:3000/api/user',
-  baseGamificationURL: '',
+  baseGamificationURL: 'http://ec2-3-238-65-233.compute-1.amazonaws.com:8080/api/gamification-event',
 };
